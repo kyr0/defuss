73053bc9295430b5f687521c4fd3108ff5d27bf3
Aron Homberg <info@aron-homberg.de>
2024-12-15 10:11:21 +0100

    chore: simplifying the deps

--- examples/with-astro-ts/astro.config.ts
+++ examples/with-astro-ts/astro.config.ts
@@ -6,16 +6,12 @@ import defuss from 'defuss-astro';
 // importing the node adapter for SSR, preview mode
 import node from '@astrojs/node';
 
-// minification
-import minify from "@frontendista/astro-html-minify";
-import purgecss from 'astro-purgecss'; // does not work with dynamic routes in Astro 5.x
-
 // https://astro.build/config
 export default defineConfig({
   // add the defuss integration to Astro
   integrations: [defuss({
     include: ['src/**/*.tsx'],
-  }), /*purgecss(), */minify()],
+  })],
 
   // the node adapter allows for server-side rendering and preview mode
   adapter: node({
