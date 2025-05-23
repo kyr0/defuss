fc2417993d18e691b4748f53fba13905de62bec0
Aron Homberg <info@aron-homberg.de>
2025-05-23 23:35:45 +0200

    chore: test build

--- examples/with-astro-ts/astro.config.ts
+++ examples/with-astro-ts/astro.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // importing the defuss-astro integration
-import defuss from 'defuss-astro';
+import defuss from "defuss-astro";
 
 // importing the node adapter for SSR, preview mode
-import node from '@astrojs/node';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   // add the defuss integration to Astro
-  integrations: [defuss({
-    include: ['src/**/*.tsx'],
-  })],
+  integrations: [
+    defuss({
+      include: ["src/**/*.tsx"],
+    }),
+  ],
 
   // the node adapter allows for server-side rendering and preview mode
   adapter: node({
-    mode: 'standalone',
+    mode: "standalone",
   }),
 });
--- packages/defuss-astro/scripts/finalize-build.ts
+++ packages/defuss-astro/scripts/finalize-build.ts
@@ -1,7 +1,7 @@
-import { readFileSync, writeFileSync } from 'node:fs';
-import { join } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { dirname } from 'node:path';
+import { readFileSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
 
 // ESM don't have __dirname, so we need to use the following workaround
 // to declare __dirname and __filename
@@ -13,20 +13,22 @@ const __dirname = dirname(__filename);
 // therefore, we can safely ignore these warnings. This function adds a comment to the dynamic import
 // that tells Vite to do so.
 function addViteIgnoreCommentToDynamicImports(fileName: string) {
+  // define the absolute path to the file, based on the current directory and relative path
+  const filePath = join(__dirname, `../dist/${fileName}`);
 
-    // define the absolute path to the file, based on the current directory and relative path
-    const filePath = join(__dirname, `../dist/${fileName}`);
+  // read the content of the file
+  const fileContent = readFileSync(filePath, "utf-8");
 
-    // read the content of the file
-    const fileContent = readFileSync(filePath, 'utf-8');
+  // make sure, Vite is not going to try to analyze the import statement when Vite is importing this file (Astro is based on Vite, so it's safe to assume)
+  const updatedContent = fileContent.replace(
+    /return import\(filepath\)/g,
+    "return import(/* @vite-ignore */ filepath)",
+  );
 
-    // make sure, Vite is not going to try to analyze the import statement when Vite is importing this file (Astro is based on Vite, so it's safe to assume)
-    const updatedContent = fileContent.replace(/return import\(filepath\)/g, "return import(/* @vite-ignore */ filepath)");
-
-    // write the updated content back to the file
-    writeFileSync(filePath, updatedContent, 'utf-8');
+  // write the updated content back to the file
+  writeFileSync(filePath, updatedContent, "utf-8");
 }
 
 // apply the function to both index.cjs and index.mjs
-addViteIgnoreCommentToDynamicImports('index.mjs');
-addViteIgnoreCommentToDynamicImports('index.cjs');
+addViteIgnoreCommentToDynamicImports("index.mjs");
+addViteIgnoreCommentToDynamicImports("index.cjs");
