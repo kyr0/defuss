504733b91a6a585310b2e01a1e442e2fa7d7a46f
Aron Homberg <info@aron-homberg.de>
2025-05-29 17:13:14 +0200

    switched to WASM based lightningimg

--- packages/defuss-astro/src/endpoint/image-endpoint.ts
+++ packages/defuss-astro/src/endpoint/image-endpoint.ts
@@ -1,74 +1,74 @@
-import type { APIRoute } from 'astro';
-import { fileTypeFromBuffer } from 'file-type';
-import { createRequire } from "node:module";
-export const nodeRequire = createRequire(import.meta.url);
-const { convertToWebp } = nodeRequire("lightningimg-node");
+import type { APIRoute } from "astro";
+import { fileTypeFromBuffer } from "file-type";
+import { convertImageBuffer, getImageInfo } from "lightningimg";
 
 export const GET: APIRoute = async ({ url }) => {
-
   try {
-    const imgHrefRelative = url.searchParams.get('href');
-    const height = url.searchParams.get('h');
-    const width = url.searchParams.get('w');
-    const quality = url.searchParams.get('q');
+    const imgHrefRelative = url.searchParams.get("href");
+    const height = url.searchParams.get("h");
+    const width = url.searchParams.get("w");
+    const quality = url.searchParams.get("q");
 
     if (!imgHrefRelative) {
-      return new Response('Image URL is required', { status: 400 });
+      return new Response("Image URL is required", { status: 400 });
     }
     const imgHrefAbsolute = `${url.protocol}//${url.host}${imgHrefRelative}`;
 
-    console.log('GET /_defuss/image (transforming: ', imgHrefAbsolute, ')');
+    console.log("GET /_defuss/image (transforming: ", imgHrefAbsolute, ")");
 
     const response = await fetch(imgHrefAbsolute);
     if (!response.ok) {
-      console.log('Error fetching image', { imgHrefAbsolute, response });
-      return new Response('Error fetching image', { status: 500 });
+      console.log("Error fetching image", { imgHrefAbsolute, response });
+      return new Response("Error fetching image", { status: 500 });
     }
     const imageBuffer = Buffer.from(await response.arrayBuffer());
-
+    const imageDimensions = await getImageInfo(imageBuffer);
     const fileExtension = (await fileTypeFromBuffer(imageBuffer))?.ext;
 
     if (!fileExtension) {
-      console.log('Error fetching fileExtension', { fileExtension, response });
-      return new Response('Error getting file extension', { status: 500 });
+      console.log("Error fetching fileExtension", { fileExtension, response });
+      return new Response("Error getting file extension", { status: 500 });
     }
 
     // if the image is already a webp, return it as is
-    if (fileExtension === 'webp') {
+    if (fileExtension === "webp") {
       return new Response(imageBuffer, {
         headers: {
-          'Content-Type': 'image/webp',
-          'Content-Length': imageBuffer.byteLength.toString(),
+          "Content-Type": "image/webp",
+          "Content-Length": imageBuffer.byteLength.toString(),
         },
       });
     }
 
     // convert the image to webp (try to)
-    let convertedImageBuffer;
+    let convertedImageBuffer: Uint8Array<ArrayBufferLike>;
     try {
-      convertedImageBuffer = convertToWebp(imageBuffer, fileExtension, {
-        height: height ? Number.parseInt(height) : undefined,
-        width: width ? Number.parseInt(width) : undefined,
-        quality: quality ? Number.parseInt(quality) : undefined,
-      });
+      convertedImageBuffer = await convertImageBuffer(
+        imageBuffer,
+        width ? Number.parseInt(width) : imageDimensions.width,
+        height ? Number.parseInt(height) : imageDimensions.height,
+      );
 
       if (!convertedImageBuffer) {
-        throw new Error('Error converting image: no buffer returned');
+        throw new Error("Error converting image: no buffer returned");
       }
     } catch (error) {
-      console.log('Error converting image', { fileExtension, bufferLength: imageBuffer.byteLength, error });
-      return new Response('Error converting image', { status: 500 });
+      console.log("Error converting image", {
+        fileExtension,
+        bufferLength: imageBuffer.byteLength,
+        error,
+      });
+      return new Response("Error converting image", { status: 500 });
     }
 
     const mimeType = `image/${fileExtension}`;
     return new Response(convertedImageBuffer, {
       headers: {
-        'Content-Type': mimeType,
-        'Content-Length': convertedImageBuffer.length.toString(),
+        "Content-Type": mimeType,
+        "Content-Length": convertedImageBuffer.length.toString(),
       },
     });
-
   } catch (error) {
     return new Response(`Error fetching image: ${error}`, { status: 500 });
   }
-}
+};
--- packages/defuss-astro/src/index.ts
+++ packages/defuss-astro/src/index.ts
@@ -1,170 +1,173 @@
-import type { AstroIntegration, AstroRenderer, ContainerRenderer, ViteUserConfig } from 'astro';
-import type { Options } from './types.js';
-import defussPlugin from 'defuss-vite';
-import { fileURLToPath } from 'node:url';
-import glob from 'fast-glob';
-import { performance } from 'node:perf_hooks'
-import { optimize } from 'svgo'
-import { join } from 'node:path';
-import { readFile, writeFile } from 'node:fs/promises';
-import { logMessage } from './log.js';
-import { PurgeCSS } from 'purgecss'
-import { existsSync } from 'node:fs';
-import { createRequire } from "node:module";
-export const nodeRequire = createRequire(import.meta.url);
-const { processDirectoryDestructive } = nodeRequire("lightningimg-node");
+import type {
+  AstroIntegration,
+  AstroRenderer,
+  ContainerRenderer,
+  ViteUserConfig,
+} from "astro";
+import type { Options } from "./types.js";
+import defussPlugin from "defuss-vite";
+import { fileURLToPath } from "node:url";
+import glob from "fast-glob";
+import { performance } from "node:perf_hooks";
+import { optimize } from "svgo";
+import { join } from "node:path";
+import { readFile, writeFile } from "node:fs/promises";
+import { logMessage } from "./log.js";
+import { PurgeCSS } from "purgecss";
+import { existsSync } from "node:fs";
 
 const getRenderer = (development: boolean): AstroRenderer => ({
-    name: 'defuss',
-    clientEntrypoint: 'defuss-astro/client.js',
-    serverEntrypoint: 'defuss-astro/server.js',
-})
+  name: "defuss",
+  clientEntrypoint: "defuss-astro/client.js",
+  serverEntrypoint: "defuss-astro/server.js",
+});
 
 export const getContainerRenderer = (): ContainerRenderer => ({
-    name: 'defuss',
-    serverEntrypoint: 'defuss-astro/server.js',
-})
+  name: "defuss",
+  serverEntrypoint: "defuss-astro/server.js",
+});
 
 const minifyAndWriteFile = async (
-	cwd: string,
-	filename: string,
-	type: 'css' | 'svg'
+  cwd: string,
+  filename: string,
+  type: "css" | "svg",
 ): Promise<void> => {
-	const filePath = join(cwd, filename)
-	const fileOrg = await readFile(filePath, 'utf8')
+  const filePath = join(cwd, filename);
+  const fileOrg = await readFile(filePath, "utf8");
 
-	switch (type) {
+  switch (type) {
+    case "svg": {
+      const fileMin = optimize(fileOrg, {
+        multipass: true,
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                removeViewBox: false,
+              },
+            },
+          },
+        ],
+      });
+      await writeFile(filePath, fileMin.data, "utf-8");
+      break;
+    }
 
-		case 'svg': {
-			const fileMin = optimize(fileOrg, {
-				multipass: true,
-				plugins: [
-					{
-						name: 'preset-default',
-						params: {
-							overrides: {
-								removeViewBox: false,
-							},
-						},
-					},
-				],
-			})
-			await writeFile(filePath, fileMin.data, 'utf-8')
-			break;
-		}
+    case "css": {
+      const purgeCSSResult = await new PurgeCSS().purge({
+        content: [`${cwd}**/*.html`],
+        css: [cwd + filename],
+        variables: false,
+      });
 
-		case 'css': {
-			const purgeCSSResult = await new PurgeCSS().purge({
-				content: [`${cwd}**/*.html`],
-				css: [cwd + filename],
-				variables: false,
-			})
+      for (const result of purgeCSSResult) {
+        if (result.file && existsSync(result.file)) {
+          await writeFile(result.file, result.css, "utf-8");
+        }
+      }
+      break;
+    }
+  }
+};
 
-			for (const result of purgeCSSResult) {
-				if (result.file && existsSync(result.file))  {
-					await writeFile(result.file, result.css, 'utf-8')
-				}
-			}
-			break;
-		}
-	}
-}
+export default function ({
+  include,
+  exclude,
+  devtools,
+}: Options = {}): AstroIntegration {
+  return {
+    name: "defuss",
+    hooks: {
+      "astro:config:setup": async ({
+        config,
+        addRenderer,
+        updateConfig,
+        command,
+        injectScript,
+        injectRoute,
+      }) => {
+        // add /_defuss/image route
+        injectRoute({
+          pattern: "/_defuss/image",
+          prerender: false,
+          entrypoint: "defuss-astro/image-endpoint.js",
+        });
 
-export default function ({ include, exclude, devtools }: Options = {}): AstroIntegration {
-	return {
-		name: 'defuss',
-		hooks: {
-			'astro:config:setup': async({ config, addRenderer, updateConfig, command, injectScript, injectRoute }) => {
-				
-				// add /_defuss/image route 
-				injectRoute({
-					pattern: '/_defuss/image',
-					prerender: false,
-					entrypoint: 'defuss-astro/image-endpoint.js',
-				});
+        const publicDirPath = fileURLToPath(config.publicDir);
 
-				const publicDirPath = fileURLToPath(config.publicDir)
+        // enable Astro's HTML compression
+        config.compressHTML = true;
+        // TODO: check: https://swc.rs/docs/usage/html
 
-				// enable Astro's HTML compression
-				config.compressHTML = true 
-				// TODO: check: https://swc.rs/docs/usage/html
+        // use CSS classes for scoped styles
+        config.scopedStyleStrategy = "class";
 
-				// use CSS classes for scoped styles
-				config.scopedStyleStrategy = 'class' 
+        if (!config.vite.build) {
+          config.vite.build = {} as ViteUserConfig["build"];
+        }
 
-				if (!config.vite.build) {
-					config.vite.build = {} as ViteUserConfig['build']
-				}
+        // use lightningcss for CSS minification
+        config.vite.build!.cssMinify = "lightningcss";
 
-				// use lightningcss for CSS minification
-				config.vite.build!.cssMinify = 'lightningcss' 
+        addRenderer(getRenderer(command === "dev"));
 
+        updateConfig({
+          vite: {
+            optimizeDeps: {
+              include: ["defuss-astro/client.js", "defuss-astro/server.js"],
+            },
+            ssr: {
+              noExternal: ["lightningimg-node"],
+            },
+            plugins: [defussPlugin()],
+          },
+        });
 
-				// convert non-webp images in public dir to webp
-				processDirectoryDestructive(publicDirPath, false)
-
-				addRenderer(getRenderer(command === 'dev'));
-
-				updateConfig({
-					vite: {
-						optimizeDeps: {
-							include: ['defuss-astro/client.js', 'defuss-astro/server.js']
-						},
-						ssr: {
-							noExternal: ["lightningimg-node"],
-						},
-						plugins: [defussPlugin()],
-					},
-				});
-
-				/**
+        /**
 				 * if (command === 'dev' && devtools) {
 					injectScript('page', 'import "preact/debug";');
 				}*/
-			},
-			'astro:build:done': async({ dir }) => {
-				const start = performance.now()
-				const cwd = fileURLToPath(dir)
-				
-				const [cssFiles, htmlFiles, imageFiles, svgFiles] = await Promise.all([
-					glob('**/*.css', { cwd, ignore: ['**/*-min.css', '**/*.min.css'] }),
-					glob('**/*.html', { cwd }),
-					glob('**/*.(jpg|jpeg|png|tiff)', { cwd }),
-					glob('**/*.svg', { cwd }),
-				])
+      },
+      "astro:build:done": async ({ dir }) => {
+        const start = performance.now();
+        const cwd = fileURLToPath(dir);
 
-				logMessage(`✴️ Optimizing ${imageFiles.length} images`)
+        const [cssFiles, htmlFiles, svgFiles] = await Promise.all([
+          glob("**/*.css", { cwd, ignore: ["**/*-min.css", "**/*.min.css"] }),
+          glob("**/*.html", { cwd }),
+          glob("**/*.svg", { cwd }),
+        ]);
 
-				processDirectoryDestructive(cwd, true)
-				
-				// SVG files without HTML files are considered as not used (in optimization context) in the project
-				// Also, when CSS is not embedded/referenced in HTML files, it is considered as not in-scope for optimization
-				if (htmlFiles.length === 0 && svgFiles.length === 0) return
+        // SVG files without HTML files are considered as not used (in optimization context) in the project
+        // Also, when CSS is not embedded/referenced in HTML files, it is considered as not in-scope for optimization
+        if (htmlFiles.length === 0 && svgFiles.length === 0) return;
 
-				logMessage(`✴️ Optimizing ${svgFiles.length} SVG vector graphics`)
-				logMessage(`✴️ Optimizing ${cssFiles.length} CSS stylesheets`)
+        logMessage(`✴️ Optimizing ${cssFiles.length} CSS stylesheets`);
 
-				const minifyAndWritePromises = []
+        const minifyAndWritePromises = [];
 
-				for (const filename of cssFiles) {
-					minifyAndWritePromises.push(minifyAndWriteFile(cwd, filename, 'css'))
-				}
+        for (const filename of cssFiles) {
+          minifyAndWritePromises.push(minifyAndWriteFile(cwd, filename, "css"));
+        }
 
-				for (const filename of svgFiles) {
-					minifyAndWritePromises.push(minifyAndWriteFile(cwd, filename, 'svg'))
-				}
+        logMessage(`✴️ Optimizing ${svgFiles.length} SVG vector graphics`);
 
-				await Promise.all(minifyAndWritePromises)
+        for (const filename of svgFiles) {
+          minifyAndWritePromises.push(minifyAndWriteFile(cwd, filename, "svg"));
+        }
 
-				const end = performance.now()
-				const deltaT = end - start
-				const humanTime =
-					deltaT < 1000
-						? `${deltaT.toFixed(0)}ms`
-						: `${(deltaT / 1000).toFixed(1)}s`
+        await Promise.all(minifyAndWritePromises);
 
-				logMessage(`✴️ Optimization completed in ${humanTime}`)
-			}
-		},
-	};
+        const end = performance.now();
+        const deltaT = end - start;
+        const humanTime =
+          deltaT < 1000
+            ? `${deltaT.toFixed(0)}ms`
+            : `${(deltaT / 1000).toFixed(1)}s`;
+
+        logMessage(`✴️ Optimization completed in ${humanTime}`);
+      },
+    },
+  };
 }
