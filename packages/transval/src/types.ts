106eef336ad8b4e524930701c443fcb8b1d5661c
Aron Homberg <info@aron-homberg.de>
2025-06-08 12:48:34 +0200

    custom message types added

--- packages/transval/src/types.ts
+++ packages/transval/src/types.ts
@@ -20,70 +20,100 @@ export interface ValidationChainApi<ET = {}> {
   not: ValidationChainApi<ET> & ET;
 
   // Basic type validators
-  isSafeNumber(): ValidationChain<ET>;
-  isString(): ValidationChain<ET>;
-  isArray(): ValidationChain<ET>;
-  isObject(): ValidationChain<ET>;
-  isDate(): ValidationChain<ET>;
-  isSafeNumeric(): ValidationChain<ET>;
-  isBoolean(): ValidationChain<ET>;
-  isInteger(): ValidationChain<ET>;
+  isSafeNumber(message?: string): ValidationChain<ET>;
+  isString(message?: string): ValidationChain<ET>;
+  isArray(message?: string): ValidationChain<ET>;
+  isObject(message?: string): ValidationChain<ET>;
+  isDate(message?: string): ValidationChain<ET>;
+  isSafeNumeric(message?: string): ValidationChain<ET>;
+  isBoolean(message?: string): ValidationChain<ET>;
+  isInteger(message?: string): ValidationChain<ET>;
 
   // Presence validators
 
-  isNull(): ValidationChain<ET>; // has no value (null)
-  isRequired(): ValidationChain<ET>; // has some value (isDefined() and !isNull() and !isEmpty())
-  isUndefined(): ValidationChain<ET>;
+  isNull(message?: string): ValidationChain<ET>; // has no value (null)
+  isRequired(message?: string): ValidationChain<ET>; // has some value (isDefined() and !isNull() and !isEmpty())
+  isUndefined(message?: string): ValidationChain<ET>;
 
-  isDefined(): ValidationChain<ET>;
-  isEmpty(): ValidationChain<ET>;
+  isDefined(message?: string): ValidationChain<ET>;
+  isEmpty(message?: string): ValidationChain<ET>;
 
   isInstanceOf<T extends new (...args: any[]) => any>(
     someConstructorFunction: T,
+    message?: string,
   ): ValidationChain<ET>; // instanceof check
 
-  isTypeOf(type: string): ValidationChain<ET>; // typeof check (e.g., "string", "number", etc.)
-  isNull(): ValidationChain<ET>; // has no value (null)
+  isTypeOf(type: string, message?: string): ValidationChain<ET>; // typeof check (e.g., "string", "number", etc.)
+  isNull(message?: string): ValidationChain<ET>; // has no value (null)
 
   // Format validators
-  isEmail(): ValidationChain<ET>;
-  isUrl(): ValidationChain<ET>;
-  isUrlPath(): ValidationChain<ET>;
-  isSlug(): ValidationChain<ET>;
-  isPhoneNumber(): ValidationChain<ET>;
-  hasDateFormat(format: string): ValidationChain<ET>;
-  hasPattern(pattern: RegExp): ValidationChain<ET>;
+  isEmail(message?: string): ValidationChain<ET>;
+  isUrl(message?: string): ValidationChain<ET>;
+  isUrlPath(message?: string): ValidationChain<ET>;
+  isSlug(message?: string): ValidationChain<ET>;
+  isPhoneNumber(message?: string): ValidationChain<ET>;
+  hasDateFormat(format: string, message?: string): ValidationChain<ET>;
+  hasPattern(pattern: RegExp, message?: string): ValidationChain<ET>;
 
   // Comparison validators
-  is(v: any): ValidationChain<ET>; // === check
+  is(v: any, message?: string): ValidationChain<ET>; // === check
   isEqual<T>(
     // must be equal to the provided value
     value: T,
+    message?: string,
   ): ValidationChain<ET>; // equals check (via JSON stringify, equalsJSON from defuss-runtime)
-  isInstanceOf<T>(c: new (...args: any[]) => T): ValidationChain<ET>; // instanceof check
-  isTypeOf(type: string): ValidationChain<ET>; // typeof check (e.g., "string", "number", etc.)
+  isInstanceOf<T>(
+    c: new (...args: any[]) => T,
+    message?: string,
+  ): ValidationChain<ET>; // instanceof check
+  isTypeOf(type: string, message?: string): ValidationChain<ET>; // typeof check (e.g., "string", "number", etc.)
   isOneOf<T extends readonly (string | number)[]>(
     // must be one of the provided values
     options: T,
+    message?: string,
   ): ValidationChain<ET>;
 
   // Boolean validators
-  isTrue(): ValidationChain<ET>; // checks if the value is true
-  isFalse(): ValidationChain<ET>; // checks if the value is false
-  isTruthy(): ValidationChain<ET>; // checks if the value is truthy
-  isFalsy(): ValidationChain<ET>; // checks if the value is falsy
+  isTrue(message?: string): ValidationChain<ET>; // checks if the value is true
+  isFalse(message?: string): ValidationChain<ET>; // checks if the value is false
+  isTruthy(message?: string): ValidationChain<ET>; // checks if the value is truthy
+  isFalsy(message?: string): ValidationChain<ET>; // checks if the value is falsy
 
   // Length validators
-  isLongerThan(minLength: number, inclusive?: boolean): ValidationChain<ET>;
-  isShorterThan(maxLength: number, inclusive?: boolean): ValidationChain<ET>;
+  isLongerThan(
+    minLength: number,
+    inclusive?: boolean,
+    message?: string,
+  ): ValidationChain<ET>;
+  isShorterThan(
+    maxLength: number,
+    inclusive?: boolean,
+    message?: string,
+  ): ValidationChain<ET>;
 
   // Numeric comparison validators
-  isGreaterThan(minValue: number, inclusive?: boolean): ValidationChain<ET>;
-  isLessThan(maxValue: number, inclusive?: boolean): ValidationChain<ET>;
+  isGreaterThan(
+    minValue: number,
+    inclusive?: boolean,
+    message?: string,
+  ): ValidationChain<ET>;
+  isLessThan(
+    maxValue: number,
+    inclusive?: boolean,
+    message?: string,
+  ): ValidationChain<ET>;
 
   // Date comparison validators
-  isAfter(minDate: Date, inclusive?: boolean): ValidationChain<ET>;
-  isBefore(maxDate: Date, inclusive?: boolean): ValidationChain<ET>;
+  isAfter(
+    minDate: Date,
+    inclusive?: boolean,
+    message?: string,
+  ): ValidationChain<ET>;
+  isBefore(
+    maxDate: Date,
+    inclusive?: boolean,
+    message?: string,
+  ): ValidationChain<ET>;
 
   // Transformers
   asString(): ValidationChain<ET>;
