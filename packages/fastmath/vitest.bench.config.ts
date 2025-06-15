4fd3f19e954dc339ec106b4af8880bd4c94ad348
Aron Homberg <info@aron-homberg.de>
2025-06-15 20:32:07 +0200

    wip

--- packages/fastmath/vitest.bench.config.ts
+++ packages/fastmath/vitest.bench.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
   test: {
     include: ["**/*.bench.ts"],
     browser: {
--- packages/fastmath/vitest.config.ts
+++ packages/fastmath/vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  },
   test: {
     exclude: ["**/*.bench.ts"],
     browser: {
