53ff2a84c08e75b25f834c6d941f9e322901059b
Aron Homberg <info@aron-homberg.de>
2025-06-23 22:13:43 +0200

    wip

--- packages/search/src/lib.rs
+++ packages/search/src/lib.rs
@@ -5,10 +5,9 @@ use wasm_bindgen::prelude::*;
 pub use wasm_bindgen_rayon::init_thread_pool;
 
 mod search;
-mod search_engine;
 
 // Re-export main types for both Rust and JavaScript usage
-pub use search_engine::{
+pub use search::{
     SearchEngine, Schema, SchemaBuilder, Document, Kind, Language,
     DocumentId, IndexError, VectorError, BM25Config, FieldWeight,
     SearchResult,
