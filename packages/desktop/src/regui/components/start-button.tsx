639c4abdb39653d583bc729da529da530d62013a
Aron Homberg <info@aron-homberg.de>
2025-07-27 16:38:05 +0200

    feat: Start button implemented

--- packages/desktop/src/regui/components/start-button.tsx
+++ packages/desktop/src/regui/components/start-button.tsx
@@ -3,6 +3,11 @@ import { StartMenu } from "./start-menu.js";
 export const StartButton = () => {
   return (
     <ul class="start-button">
+      <img
+        class="start-button-icon"
+        src="/defuss_xp_quad.webp"
+        alt="Start Icon"
+      />
       <a href="#">Start </a>
       <StartMenu />
     </ul>
