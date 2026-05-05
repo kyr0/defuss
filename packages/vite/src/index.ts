234ebc76713156c4ffdb7dbab4651955acaff1b6
Aron Homberg <info@aron-homberg.de>
2026-05-05 19:06:19 +0200

    feat: vite v8

--- packages/vite/src/index.ts
+++ packages/vite/src/index.ts
@@ -5,123 +5,125 @@ import type { DefussVitePluginOptions } from "./types.js";
 
 // Allows to ignore query parameters, as in Vue SFC virtual modules.
 function parseId(url: string) {
-  return { id: url.split("?", 2)[0] };
+	return { id: url.split("?", 2)[0] };
 }
 
 export interface ExistingRawSourceMap {
-  file?: string;
-  mappings: string;
-  names: string[];
-  sourceRoot?: string;
-  sources: string[];
-  sourcesContent?: string[];
-  version: number;
-  x_google_ignoreList?: number[];
+	file?: string;
+	mappings: string;
+	names: string[];
+	sourceRoot?: string;
+	sources: string[];
+	sourcesContent?: string[];
+	version: number;
+	x_google_ignoreList?: number[];
 }
 
 export type MaybeSourceMap =
-  | ExistingRawSourceMap
-  | string
-  | null
-  | { mappings: "" };
+	| ExistingRawSourceMap
+	| string
+	| null
+	| { mappings: "" };
 
 // makes sure that the JSX pragma is set to `tsx` and the JSX fragment pragma is set to `Fragment`
 // so that JSX is correctly transpiled and tsx() is called to construct the virtual DOM call tree (functional AST)
 export default function defussVitePlugin({
-  include,
-  exclude,
+	include,
+	exclude,
 	enableJsxDevMode
 }: DefussVitePluginOptions = {}): PluginOption {
-  let config: ResolvedConfig;
+	let config: ResolvedConfig;
 	const shouldTransform = createFilter(
-    include || [/\.[cm]?[tj]sx?$/],
-    exclude || [/node_modules/],
-  );
+		include || [/\.[cm]?[tj]sx?$/],
+		exclude || [/node_modules/],
+	);
 
-  const jsxPlugin: PluginOption = {
-    name: "vite:defuss-jsx",
-    enforce: "pre",
-    config() {
-      return {
-        esbuild: {
-          // we want Vite to handle JSX transformations
-          jsxImportSource: "defuss",
-          jsxFactory: "jsx",
-          jsxFragment: "Fragment",
-          jsxDev: true, // enable sourceInfo for better hydration and debugging
-        },
-        optimizeDeps: {
-          // we want Vite to optimize the runtime code
-          include: ["defuss"],
-          // we don't want Vite to optimize this plugin code
-          exclude: ["defuss-vite"],
-        },
-        ssr: {
-          // LinkeDOM (used by defuss/render) imports perf_hooks
-          // which is actually just an empty placeholder package,
-          // therefore, we externalize it
-          external: ["perf_hooks"],
-        },
-      };
-    },
-    configResolved(resolvedConfig) {
-      config = resolvedConfig;
-    },
-    async transform(code, url) {
+	const jsxPlugin: PluginOption = {
+		name: "vite:defuss-jsx",
+		enforce: "pre",
+		config() {
+			return {
+				oxc: {
+					// we want Vite to handle JSX transformations
+					jsx: {
+						importSource: "defuss",
+						factory: "jsx",
+						fragment: "Fragment",
+						development: true, // enable sourceInfo for better hydration and debugging
+					},
+				},
+				optimizeDeps: {
+					// we want Vite to optimize the runtime code
+					include: ["defuss"],
+					// we don't want Vite to optimize this plugin code
+					exclude: ["defuss-vite"],
+				},
+				ssr: {
+					// LinkeDOM (used by defuss/render) imports perf_hooks
+					// which is actually just an empty placeholder package,
+					// therefore, we externalize it
+					external: ["perf_hooks"],
+				},
+			};
+		},
+		configResolved(resolvedConfig) {
+			config = resolvedConfig;
+		},
+		async transform(code, url) {
 
 			const isDevMode =
 				enableJsxDevMode !== undefined
-					? enableJsxDevMode                        
+					? enableJsxDevMode
 					: config.command === "serve";
 
 			const { id } = parseId(url);
 
-      if (!shouldTransform(id)) {
-        return null;
-      }
+			if (!shouldTransform(id)) {
+				return null;
+			}
 
-      const prevSourceMap = this.getCombinedSourcemap?.();
-      const inputSourceMap = prevSourceMap
-        ? JSON.stringify({ code, map: prevSourceMap })
-        : undefined;
+			const prevSourceMap = this.getCombinedSourcemap?.();
+			const inputSourceMap = prevSourceMap
+				? JSON.stringify({ code, map: prevSourceMap })
+				: undefined;
 
-      const result = await transform(code, {
-        jsc: {
-          parser: {
-            syntax: "typescript",
-            tsx: true,
-            dynamicImport: true,
-            decorators: true,
-          },
-          transform: {
-            react: {
-              development: isDevMode,
-              useBuiltins: false,
-              throwIfNamespace: false,
-              pragma: "jsx",
-              pragmaFrag: "Fragment",
-              runtime: "automatic",
-              importSource: "defuss",
-            },
-          },
-          target: "es2024",
-        },
-        sourceMaps: true,
-        sourceFileName: id,
-        filename: id,
-        sourceRoot: config.root,
-        inputSourceMap,
-      });
+			const result = await transform(code, {
+				jsc: {
+					parser: {
+						syntax: "typescript",
+						tsx: true,
+						dynamicImport: true,
+						decorators: true,
+					},
+					transform: {
+						react: {
+							development: isDevMode,
+							useBuiltins: false,
+							throwIfNamespace: false,
+							pragma: "jsx",
+							pragmaFrag: "Fragment",
+							runtime: "automatic",
+							importSource: "defuss",
+						},
+					},
+					target: "es2024",
+				},
+				sourceMaps: true,
+				sourceFileName: id,
+				filename: id,
+				sourceRoot: config.root,
+				inputSourceMap,
+			});
 
-      if (!result) return null;
+			if (!result) return null;
 
-      return {
-        code: result.code || code,
-        map: result.map,
-      };
-    },
-  };
-  return jsxPlugin as PluginOption;
+			return {
+				code: result.code || code,
+				map: result.map,
+			};
+		},
+	};
+	return jsxPlugin as PluginOption;
 }
 
 export * from "./types.js";
